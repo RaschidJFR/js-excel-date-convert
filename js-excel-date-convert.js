11f4b2af68b909219ed46613db37ce5d992ea50a
Raschid J.F. Rafaelly <me@raschidjfr.dev>
2019-12-06 12:38:16 -0600

    refactor: use  `module.exports` instead of `export function`

--- js-excel-date-convert.js
+++ js-excel-date-convert.js
@@ -5,7 +5,7 @@
  * @param {boolean} [date1904] Whether to use the 1904 Date System. See https://bettersolutions.com/excel/dates-times/1904-date-system.htm
  * @author Raschid JF Rafaelly <hello&commat;raschidjdr.dev>
  */
-export function fromExcelDate (excelDate, date1904) {
+function fromExcelDate (excelDate, date1904) {
   const daysIn4Years = 1461;
   const daysIn70years = Math.round(25567.5 + 1); // +1 because of the leap-year bug
   const daysFrom1900 = excelDate + (date1904 ? daysIn4Years + 1 : 0);
@@ -21,7 +21,7 @@ export function fromExcelDate (excelDate, date1904) {
  * @param {boolean} [date1904] Whether to use the 1904 Date System. See https://bettersolutions.com/excel/dates-times/1904-date-system.htm
  * @author Raschid JF Rafaelly <hello&commat;raschidjdr.dev>
  */
-export function toExcelDate (date, date1904) {
+function toExcelDate (date, date1904) {
   // see https://bettersolutions.com/excel/dates-times/1904-date-system.htm
   if (isNaN(date)) return null;
   const daysIn4Years = 1461;
@@ -31,3 +31,8 @@ export function toExcelDate (date, date1904) {
   const daysFrom1904Jan2nd = daysFrom1900 - daysIn4Years - 1;
   return Math.round(date1904 ? daysFrom1904Jan2nd : daysFrom1900);
 }
+
+module.exports = {
+  fromExcelDate,
+  toExcelDate
+};
